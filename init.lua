2988ce7e40e39ceea8becc395b517acca544401e
wogara <weogara@gmail.com>
2024-06-29 00:54:45 -0400

    added colorschemes

--- init.lua
+++ init.lua
@@ -14,3 +14,42 @@ vim.opt.clipboard:append("unnamedplus")
 
 require("vim-options")
 require("lazy").setup("plugins")
+local theme_file = vim.fn.stdpath("config") .. "/current_theme"
+function _G.switch_theme(theme)
+  local file = io.open(theme_file, "w")
+  file:write(theme)
+  file:close()
+
+  if theme == "catppuccin" then
+    require("catppuccin").setup({
+      flavour = "mocha",
+    })
+    vim.cmd.colorscheme("catppuccin-mocha")
+  elseif theme == "gruvbox" then
+    require("gruvbox").setup({
+      transparent_mode = false,
+    })
+    vim.cmd.colorscheme("gruvbox")
+  elseif theme == "dracula" then
+    require("dracula").setup({})
+    vim.cmd.colorscheme("dracula")
+  elseif theme == "tokyonight" then
+    require("tokyonight").setup({})
+    vim.cmd.colorscheme("tokyonight")
+  else
+    require("catppuccin").setup({
+      flavour = "mocha",
+    })
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end
+end
+
+local file = io.open(theme_file, "r")
+local initial_theme = file and file:read("*all") or "catppuccin"
+if file then
+  file:close()
+end
+
+initial_theme = initial_theme:gsub("%s+", "")
+
+switch_theme(initial_theme)
---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,10 @@
+return {
+	"folke/tokyonight.nvim",
+	name = "tokyonight",
+	priority = 1000,
+	config = function()
+    require("tokyonight").setup({
+    })
+		vim.cmd[[colorscheme tokyonight]]
+	end,
+}
